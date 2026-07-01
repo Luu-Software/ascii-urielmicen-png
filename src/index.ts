02387fb30fd266bc4d8c9480c3e6f34e4795e6e7
urielmicen-png <urielmicen@gmail.com>
2026-07-01 15:52:14 -0300

    consigna base terminada

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+  let resultado = '';
+  let contador = 0;
+  while (contador < cantidad) {
+    resultado += letra;
+    contador++;
+  }
+  return resultado;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+for (let i = 0; i < tamaño; i++) {
+  console.log(repetirLetra(caracter, tamaño));
+}
